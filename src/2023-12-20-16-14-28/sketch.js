29d986640fd0dd74e21ea906aea0bfe52d3bfd5e
koya-kimura <2ktmnri8@gmail.com>
2023-12-20 17:27:06 +0900

    2023/12/20-17:27:06 save

---
+++ src/2023-12-20-16-14-28/sketch.js
@@ -0,0 +1,185 @@
+let pg = [];
+let mpg;
+let ft;
+
+function preload() {
+  ft = loadFont("../../assets/font/NotoSans_Condensed-BoldItalic.ttf")
+}
+
+function setup() {
+  createCanvas(1280, 720);
+  imageMode(CENTER);
+
+  mpg = createGraphics(width, height);
+  mpg.background(0);
+  mpg.textFont(ft);
+  mpg.textAlign(CENTER);
+  mpg.fill(255);
+  mpg.textSize(height / 3);
+  mpg.text("Hello World \n p5.js", width / 2, height / 2 - 60);
+
+  for (let i in colorPalletes) {
+    pg[i] = createGraphics(min(width, height), min(width, height));
+    pg[i].background(0);
+    pg[i].rectMode(CENTER);
+
+    const cp = colorPalletes[i].colors;
+
+    for (let j = 0; j < 100; j++) {
+      if (random(100) < 50) {
+        pg[i].fill(random(cp));
+        pg[i].noStroke();
+      } else {
+        pg[i].noFill();
+        pg[i].stroke(random(cp));
+      }
+
+      const s = random(0.2, 0.6) * pg[i].height;
+      pg[i].rect(random(pg[i].width), random(pg[i].height), s, s);
+    }
+  }
+
+  const grid = 20;
+  for (let x = grid / 2; x < width; x += grid) {
+    for (let y = grid / 2; y < height; y += grid) {
+      let a = mpg.get(x, y);
+      if (red(a) > 0) {
+        tint(255);
+        image(pg[0], x, y, grid*2.5, grid*2.5);
+      } else {
+        tint(255, 200);
+        image(pg[floor(random(1, pg.length))], x, y, grid, grid);
+      }
+    }
+  }
+}
+
+class Easing {
+  static easeInSine(x) {
+    return 1 - Math.cos((x * Math.PI) / 2);
+  }
+
+  static easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
+  }
+
+  static easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+  }
+
+  static easeInQuad(x) {
+    return x * x;
+  }
+
+  static easeOutQuad(x) {
+    return 1 - (1 - x) * (1 - x);
+  }
+
+  static easeInOutQuad(x) {
+    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+  }
+
+  static easeInCubic(x) {
+    return x * x * x;
+  }
+
+  static easeOutCubic(x) {
+    return 1 - Math.pow(1 - x, 3);
+  }
+
+  static easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+  }
+
+  static easeInQuart(x) {
+    return x * x * x * x;
+  }
+
+  static easeOutQuart(x) {
+    return 1 - Math.pow(1 - x, 4);
+  }
+
+  static easeInOutQuart(x) {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+  }
+
+  static easeInQuint(x) {
+    return x * x * x * x * x;
+  }
+
+  static easeOutQuint(x) {
+    return 1 - Math.pow(1 - x, 5);
+  }
+
+  static easeInOutQuint(x) {
+    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+  }
+
+  static easeInExpo(x) {
+    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+  }
+
+  static easeOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+  }
+
+  static easeInOutExpo(x) {
+    return x === 0 ?
+      0 :
+      x === 1 ?
+      1 :
+      x < 0.5 ?
+      Math.pow(2, 20 * x - 10) / 2 :
+      (2 - Math.pow(2, -20 * x + 10)) / 2;
+  }
+
+  static easeInCirc(x) {
+    return 1 - Math.sqrt(1 - Math.pow(x, 2));
+  }
+
+  static easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 2));
+  }
+
+  static easeInOutCirc(x) {
+    return x < 0.5 ?
+      (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 :
+      (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+  }
+
+  static easeOutBack(x) {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+  }
+
+  static easeInOutBack(x) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+    return x < 0.5 ?
+      (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 :
+      (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+  }
+}
+
+const colorPalletes = [{
+    name: "DeepEmeraldGold",
+    colors: ["#005e55", "#fff9bf", "#edb50c", "#b8003d", "#5e001f"],
+  },
+  {
+    name: "WarmRainbow",
+    colors: ["#01204E", "#028391", "#F6DCAC", "#FAA968", "#F85525"],
+  },
+  {
+    name: "ChocolateAndCream",
+    colors: ["#D54751", "#EF9A48", "#FFFCC7", "#4DA394", "#59322B"],
+  },
+  {
+    name: "PopArt",
+    colors: ["#241965", "#653993", "#9F4094", "#B73D6E", "#F19406"],
+  },
+  {
+    name: "DeepEmeraldGold",
+    colors: ["#F87523", "#FFC31B", "#E7DCC9", "#1DB7B9", "#126D68"],
+  },
+];

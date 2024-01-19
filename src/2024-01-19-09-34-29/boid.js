42528b1a672fd6d136824a22edaef9fb0393d730
koya-kimura <2ktmnri8@gmail.com>
2024-01-19 10:58:33 +0900

    2024/01/19-10:58:33 save

---
+++ src/2024-01-19-09-34-29/boid.js
@@ -0,0 +1,132 @@
+class Boid {
+    constructor(x, y, cp) {
+        this.position = createVector(x, y);
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, 4));
+        this.acceleration = createVector();
+        this.maxForce = 0.2;
+        this.maxSpeed = 5;
+        this.s = 15 * floor(random(1, 4));
+        this.cp = cp;
+        this.index = floor(random(this.cp.length));
+    }
+
+    edges() {
+        if (this.position.x > width) {
+            this.position.x = 0;
+        } else if (this.position.x < 0) {
+            this.position.x = width;
+        }
+        if (this.position.y > height) {
+            this.position.y = 0;
+        } else if (this.position.y < 0) {
+            this.position.y = height;
+        }
+    }
+
+    align(boids) {
+        let perceptionRadius = 25;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < perceptionRadius) {
+                steering.add(other.velocity);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    separation(boids) {
+        let perceptionRadius = 24;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < perceptionRadius) {
+                let diff = p5.Vector.sub(this.position, other.position);
+                diff.div(d * d);
+                steering.add(diff);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    cohesion(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < perceptionRadius) {
+                steering.add(other.position);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.sub(this.position);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    flock(boids) {
+        let alignment = this.align(boids);
+        let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+
+        alignment.mult(alignSlider.value());
+        cohesion.mult(cohesionSlider.value());
+        separation.mult(separationSlider.value());
+
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+        this.acceleration.add(separation);
+
+
+        let perceptionRadius = 30;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < perceptionRadius) {
+                this.index = other.index;
+            }
+        }
+    }
+
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.acceleration.mult(0);
+
+        if (random(1000) < 10) {
+            this.index = floor(random(this.cp.length));
+        }
+    }
+
+    show() {
+        const x = floor(this.position.x / this.s) * this.s;
+        const y = floor(this.position.y / this.s) * this.s;
+
+        noStroke();
+        fill(this.cp[this.index]);
+        rect(x, y, this.s, this.s);
+    }
+}

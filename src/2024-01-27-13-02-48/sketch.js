f70a778ff49f3cf012f3760605048560122e858e
koya-kimura <2ktmnri8@gmail.com>
2024-01-27 15:10:26 +0900

    2024/01/27-15:10:26 save

---
+++ src/2024-01-27-13-02-48/sketch.js
@@ -0,0 +1,201 @@
+/* Znを何回漸化式で計算するか */
+let N = 255;
+/* Znの最大値 */
+let L = 255;
+/* 拡大率 */
+let SCALE = 3.0;
+
+function setup() {
+  createCanvas(512, 512);
+}
+
+function draw() {
+  /*中心に原点を持ってくる*/
+  translate(width / 2, height / 2);
+  background(0);
+
+  for (let a = -width / 2; a <= width / 2; a++) {
+    for (let b = -height / 2; b <= height / 2; b++) {
+      /* 複素数c=x+yiを定める */
+      let x = SCALE * a / width;
+      let y = SCALE * b / height;
+      /* 漸化式に基づいて計算 */
+      let r = calc(x, y);
+
+      /* 収束の速度に応じて色を指定 */
+      /* 下のプログラムは適当。stroke(赤成分緑成分,青成分) */
+      stroke(r % 256, r * 4 % 256, r * 16 % 256);
+      /* 点の描画 */
+      rect(a, b, 1, 1);
+    }
+  }
+}
+
+/* 漸化式にもとづいてZnが発散するnを計算 */
+function calc(x, y) {
+  let tx, ty;
+  let zx = 0.0;
+  let zy = 0.0;
+
+  for (let i = 1; i <= N; i++) {
+    tx = zx;
+    ty = zy;
+
+    zx = tx * tx - ty * ty + x;
+    zy = 2 * tx * ty + y;
+
+    /* 発散した（閾値を超えた）場合はiを返す */
+    if (zx * zx + zy * zy > L)
+      return i;
+  }
+
+  /* 発散しなかった場合は0を返す */
+  return 0;
+}
+
+class Easing {
+  static easeInSine(x) {
+    return 1 - Math.cos((x * Math.PI) / 2);
+  }
+
+  static easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
+  }
+
+  static easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+  }
+
+  static easeInQuad(x) {
+    return x * x;
+  }
+
+  static easeOutQuad(x) {
+    return 1 - (1 - x) * (1 - x);
+  }
+
+  static easeInOutQuad(x) {
+    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+  }
+
+  static easeInCubic(x) {
+    return x * x * x;
+  }
+
+  static easeOutCubic(x) {
+    return 1 - Math.pow(1 - x, 3);
+  }
+
+  static easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+  }
+
+  static easeInQuart(x) {
+    return x * x * x * x;
+  }
+
+  static easeOutQuart(x) {
+    return 1 - Math.pow(1 - x, 4);
+  }
+
+  static easeInOutQuart(x) {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+  }
+
+  static easeInQulet(x) {
+    return x * x * x * x * x;
+  }
+
+  static easeOutQulet(x) {
+    return 1 - Math.pow(1 - x, 5);
+  }
+
+  static easeInOutQulet(x) {
+    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+  }
+
+  static easeInExpo(x) {
+    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+  }
+
+  static easeOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+  }
+
+  static easeInOutExpo(x) {
+    return x === 0 ?
+      0 :
+      x === 1 ?
+      1 :
+      x < 0.5 ?
+      Math.pow(2, 20 * x - 10) / 2 :
+      (2 - Math.pow(2, -20 * x + 10)) / 2;
+  }
+
+  static easeInCirc(x) {
+    return 1 - Math.sqrt(1 - Math.pow(x, 2));
+  }
+
+  static easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 2));
+  }
+
+  static easeInOutCirc(x) {
+    return x < 0.5 ?
+      (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 :
+      (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+  }
+
+  static easeOutBack(x) {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+  }
+
+  static easeInOutBack(x) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+    return x < 0.5 ?
+      (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 :
+      (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+  }
+}
+
+const colorPalletes = [
+  {
+    name: "DeepEmeraldGold",
+    colors: ["#005e55", "#fff9bf", "#edb50c", "#b8003d", "#5e001f"],
+  },
+  {
+    name: "WarmRainbow",
+    colors: ["#01204E", "#028391", "#F6DCAC", "#FAA968", "#F85525"],
+  },
+  {
+    name: "ChocolateAndCream",
+    colors: ["#D54751", "#EF9A48", "#FFFCC7", "#4DA394", "#59322B"],
+  },
+  {
+    name: "PopArt",
+    colors: ["#241965", "#653993", "#9F4094", "#B73D6E", "#F19406"],
+  },
+  {
+    name: "DeepEmeraldGold",
+    colors: ["#F87523", "#FFC31B", "#E7DCC9", "#1DB7B9", "#126D68"],
+  },
+  {
+    name: "GreenPink",
+    colors: ["#01B999", "#FAB3B3", "#DC958F", "#A1D8CE", "#F1FAF7"],
+  },
+  {
+    name: "NatureTranquility",
+    colors: ["#106A6B", "#07374B", "#CAB381", "#E9E0CE"],
+  },
+  {
+    name: "VibrantHarmony",
+    colors: ["#F15946", "#5681CB", "#FAAA2D", "#296647", "#453945"],
+  },
+  {
+    name: "Serenity Bliss",
+    colors: ["#FFB4B8", "#EF4B28", "#0A563A", "#FFBC54", "#ECE9E0"],
+  }
+];

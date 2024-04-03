2ed1ae65464e07b9175b0b1ab8e1a061ece941cc
koya-kimura <2ktmnri8@gmail.com>
2024-04-03 17:21:02 +0900

    2024/04/03-17:21:02 save

---
+++ src/2024-04-03-16-59-37/sketch.js
@@ -0,0 +1,192 @@
+const s = 300;
+let pg;
+
+const n = 8000;
+
+let pos = [];
+let c = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  pg = createGraphics(s*10, s*10);
+  // pg.background(255);
+
+  for (let i = 0; i < n; i++) {
+    pos[i] = createVector(random(pg.width), random(pg.height));
+    c[i] = random(255);
+  }
+}
+
+function draw() {
+  background(10);
+
+  camera(1000 * cos(frameCount / 300), -300, 1000 * sin(frameCount / 300), 0, 0, 0, 0, 1, 0);
+
+  ambientLight(200);
+  pointLight(255, 0, 0, 1000 * cos(frameCount / 150), -300, 1000 * sin(frameCount / 150));
+  pointLight(255, 255, 0, 1000 * cos(frameCount / 200), 300, 1000 * sin(frameCount / 200));
+  pointLight(0, 255, 0, 1000 * cos(frameCount / 100), 100, 1000 * sin(frameCount / 100));
+
+  for (let i in pos) {
+    const d = createVector(noise(pos[i].x / pg.width * 15, pos[i].y / pg.height * 15, 0.1) - 0.5, noise(pos[i].x / pg.width * 15, pos[i].y / pg.height * 15, 10.2) - 0.5);
+    d.mult(10);
+
+    pos[i].add(d);
+
+    pg.strokeWeight(5*noise(frameCount/100, i));
+    pg.stroke(c[i]);
+    pg.point(pos[i].x, pos[i].y);
+  }
+
+  noStroke();
+  texture(pg);
+  sphere(s);
+}
+
+class Easing {
+  static easeInSine(x) {
+    return 1 - Math.cos((x * Math.PI) / 2);
+  }
+
+  static easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
+  }
+
+  static easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+  }
+
+  static easeInQuad(x) {
+    return x * x;
+  }
+
+  static easeOutQuad(x) {
+    return 1 - (1 - x) * (1 - x);
+  }
+
+  static easeInOutQuad(x) {
+    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+  }
+
+  static easeInCubic(x) {
+    return x * x * x;
+  }
+
+  static easeOutCubic(x) {
+    return 1 - Math.pow(1 - x, 3);
+  }
+
+  static easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+  }
+
+  static easeInQuart(x) {
+    return x * x * x * x;
+  }
+
+  static easeOutQuart(x) {
+    return 1 - Math.pow(1 - x, 4);
+  }
+
+  static easeInOutQuart(x) {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+  }
+
+  static easeInQuint(x) {
+    return x * x * x * x * x;
+  }
+
+  static easeOutQuint(x) {
+    return 1 - Math.pow(1 - x, 5);
+  }
+
+  static easeInOutQuint(x) {
+    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+  }
+
+  static easeInExpo(x) {
+    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+  }
+
+  static easeOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+  }
+
+  static easeInOutExpo(x) {
+    return x === 0 ?
+      0 :
+      x === 1 ?
+      1 :
+      x < 0.5 ?
+      Math.pow(2, 20 * x - 10) / 2 :
+      (2 - Math.pow(2, -20 * x + 10)) / 2;
+  }
+
+  static easeInCirc(x) {
+    return 1 - Math.sqrt(1 - Math.pow(x, 2));
+  }
+
+  static easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 2));
+  }
+
+  static easeInOutCirc(x) {
+    return x < 0.5 ?
+      (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 :
+      (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+  }
+
+  static easeOutBack(x) {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+  }
+
+  static easeInOutBack(x) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+    return x < 0.5 ?
+      (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 :
+      (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+  }
+}
+
+const colorPalletes = [
+  {
+    name: "DeepEmeraldGold",
+    colors: ["#005e55", "#fff9bf", "#edb50c", "#b8003d", "#5e001f"],
+  },
+  {
+    name: "WarmRainbow",
+    colors: ["#01204E", "#028391", "#F6DCAC", "#FAA968", "#F85525"],
+  },
+  {
+    name: "ChocolateAndCream",
+    colors: ["#D54751", "#EF9A48", "#FFFCC7", "#4DA394", "#59322B"],
+  },
+  {
+    name: "PopArt",
+    colors: ["#241965", "#653993", "#9F4094", "#B73D6E", "#F19406"],
+  },
+  {
+    name: "DeepEmeraldGold",
+    colors: ["#F87523", "#FFC31B", "#E7DCC9", "#1DB7B9", "#126D68"],
+  },
+  {
+    name: "GreenPink",
+    colors: ["#01B999", "#FAB3B3", "#DC958F", "#A1D8CE", "#F1FAF7"],
+  },
+  {
+    name: "NatureTranquility",
+    colors: ["#106A6B", "#07374B", "#CAB381", "#E9E0CE"],
+  },
+  {
+    name: "VibrantHarmony",
+    colors: ["#F15946", "#5681CB", "#FAAA2D", "#296647", "#453945"],
+  },
+  {
+    name: "Serenity Bliss",
+    colors: ["#FFB4B8", "#EF4B28", "#0A563A", "#FFBC54", "#ECE9E0"],
+  }
+];
